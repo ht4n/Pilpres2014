e0611e513ed846ec8f638602ea7425b3ad968801
Henry Tan <ht4n@hotmail.com>
2014-07-17 03:08:34 -0700

    "JSON formatting fix"

--- VoteCounter/Program.cs
+++ VoteCounter/Program.cs
@@ -97,7 +97,7 @@ namespace Pilpres2014
         static int s_itemCount = 0;
 
         // This variable is for debugging. If it is not for debugging set this to UINT32_MAX
-        static UInt32 s_maxItems = 100;
+        static UInt32 s_maxItems = UInt32.MaxValue;
 
         static bool ParseCandidateVoteCount(String filterStart, String filterEnd, String resultPage, out UInt64 voteCount)
         {
