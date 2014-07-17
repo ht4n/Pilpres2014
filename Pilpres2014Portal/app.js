f3583768923f386c7e5414abc00b1aa1d2b60835
Henry Tan <ht4n@hotmail.com>
2014-07-17 04:27:08 -0700

    Checkin pilpres2014 watchdog portal

---
+++ Pilpres2014Portal/app.js
@@ -0,0 +1,99 @@
+﻿///<reference path="Scripts/typings/jquery/jquery.d.ts"/>
+///<reference path="Scripts/typings/knockout/knockout.d.ts"/>
+var VoteEntry = (function () {
+    function VoteEntry() {
+        this.counter1 = ko.observable(0);
+        this.counter1Percentage = ko.observable(0);
+        this.counter2 = ko.observable(0);
+        this.counter2Percentage = ko.observable(0);
+        this.total = ko.observable(0);
+        this.label = ko.observable("");
+    }
+    return VoteEntry;
+})();
+
+var Pilpres2014 = (function () {
+    function Pilpres2014() {
+        this.url = ko.observable("https://github.com/ht4n/Pilpres2014");
+        this.provinces = ko.observableArray([]);
+        this.totalVotes1 = ko.observable(0);
+        this.totalVotes2 = ko.observable(0);
+        this.percentageVotes1 = ko.observable("0%");
+        this.percentageVotes2 = ko.observable("0%");
+        this.totalVotes = ko.observable(0);
+        this.voteEntries = ko.observableArray([]);
+    }
+    Pilpres2014.prototype.refresh = function () {
+        var self = this;
+
+        var totalCallback = function (data, status) {
+            console.log("response:" + status);
+            if (status !== "success") {
+                return;
+            }
+
+            self.voteEntries.removeAll();
+            data.forEach(function (entry) {
+                var voteEntry = new VoteEntry();
+                voteEntry.counter1 = entry.PrabowoHattaVotes;
+                voteEntry.counter1Percentage = entry.PrabowoHattaPercentage;
+                voteEntry.counter2 = entry.JokowiKallaVotes;
+                voteEntry.counter2Percentage = entry.JokowiKallaPercentage;
+
+                self.totalVotes(entry.Total);
+                self.totalVotes1(entry.PrabowoHattaVotes);
+                self.totalVotes2(entry.JokowiKallaVotes);
+                self.percentageVotes1(entry.PrabowoHattaPercentage + "%");
+                self.percentageVotes2(entry.JokowiKallaPercentage + "%");
+            });
+        };
+
+        this.query("KPU-Feeds-2014-07-17-03-AM-total.json", null, totalCallback);
+
+        var provinceCallback = function (data, status) {
+            console.log("response:" + status);
+            if (status !== "success") {
+                return;
+            }
+
+            self.voteEntries.removeAll();
+            var response = data.response;
+            response.forEach(function (entry) {
+                var voteEntry = new VoteEntry();
+                voteEntry.counter1 = entry.PrabowoHattaVotes;
+                voteEntry.counter1Percentage = entry.PrabowoHattaPercentage;
+                voteEntry.counter2 = entry.PrabowoHattaVotes;
+                voteEntry.counter2Percentage = entry.JokowiKallaPercentage;
+                voteEntry.label = entry.Province;
+
+                self.voteEntries.push(voteEntry);
+            });
+        };
+
+        this.query("KPU-Feeds-2014-07-17-03-AM-province.json", null, provinceCallback);
+    };
+
+    Pilpres2014.prototype.query = function (url, context, callback, statusCallback) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            contentType: 'application/json',
+            context: context,
+            statusCode: statusCallback
+        }).always(function (data, status, jqXHR) {
+            if (callback) {
+                callback.call(this, data, status, jqXHR);
+            }
+        });
+    };
+    return Pilpres2014;
+})();
+
+window.onload = function () {
+    var pilpres2014ViewModel = new Pilpres2014();
+    ko.applyBindings(pilpres2014ViewModel);
+
+    pilpres2014ViewModel.refresh();
+};
+//# sourceMappingURL=app.js.map
---
+++ Pilpres2014Portal/app.ts
@@ -0,0 +1,114 @@
+﻿///<reference path="Scripts/typings/jquery/jquery.d.ts"/>
+///<reference path="Scripts/typings/knockout/knockout.d.ts"/>
+
+class VoteEntry {
+    counter1: KnockoutObservable<number>;
+    counter1Percentage: KnockoutObservable<number>;
+    counter2: KnockoutObservable<number>;
+    counter2Percentage: KnockoutObservable<number>;
+    total: KnockoutObservable<number>;
+    label: KnockoutObservable<string>;
+
+    constructor() {
+        this.counter1 = ko.observable(0);
+        this.counter1Percentage = ko.observable(0);
+        this.counter2 = ko.observable(0);
+        this.counter2Percentage = ko.observable(0);
+        this.total = ko.observable(0);
+        this.label = ko.observable("");
+    }
+}
+
+class Pilpres2014 {
+    provinces: KnockoutObservableArray<string>;
+    url: KnockoutObservable<string>;
+    totalVotes1: KnockoutObservable<number>;
+    totalVotes2: KnockoutObservable<number>;
+    percentageVotes1: KnockoutObservable<string>;
+    percentageVotes2: KnockoutObservable<string>;
+    totalVotes: KnockoutObservable<number>;
+    voteEntries: KnockoutObservableArray<VoteEntry>;
+
+    constructor() {
+        this.url = ko.observable("https://github.com/ht4n/Pilpres2014");
+        this.provinces = ko.observableArray([]);        
+        this.totalVotes1 = ko.observable(0);
+        this.totalVotes2 = ko.observable(0);
+        this.percentageVotes1 = ko.observable("0%");
+        this.percentageVotes2 = ko.observable("0%");
+        this.totalVotes = ko.observable(0);
+        this.voteEntries = ko.observableArray([]);
+    }
+
+    refresh() {
+        var self = this;
+
+        var totalCallback = function (data, status) {
+            console.log("response:" + status);
+            if (status !== "success") {
+                return;
+            }
+
+            self.voteEntries.removeAll();
+            data.forEach((entry) => {
+                var voteEntry = new VoteEntry();
+                voteEntry.counter1 = entry.PrabowoHattaVotes;                
+                voteEntry.counter1Percentage = entry.PrabowoHattaPercentage;
+                voteEntry.counter2 = entry.JokowiKallaVotes;
+                voteEntry.counter2Percentage = entry.JokowiKallaPercentage;
+
+                self.totalVotes(entry.Total);
+                self.totalVotes1(entry.PrabowoHattaVotes);
+                self.totalVotes2(entry.JokowiKallaVotes);
+                self.percentageVotes1(entry.PrabowoHattaPercentage + "%");
+                self.percentageVotes2(entry.JokowiKallaPercentage + "%");
+            });
+        }
+
+        this.query("KPU-Feeds-2014-07-17-03-AM-total.json", null, totalCallback);
+
+        var provinceCallback = function (data, status) {
+            console.log("response:" + status);
+            if (status !== "success") {
+                return;
+            }
+
+            self.voteEntries.removeAll();
+            var response = data.response;
+            response.forEach((entry) => {
+                var voteEntry = new VoteEntry();
+                voteEntry.counter1 = entry.PrabowoHattaVotes;
+                voteEntry.counter1Percentage = entry.PrabowoHattaPercentage;
+                voteEntry.counter2 = entry.PrabowoHattaVotes;
+                voteEntry.counter2Percentage = entry.JokowiKallaPercentage;
+                voteEntry.label = entry.Province;
+
+                self.voteEntries.push(voteEntry);
+            });
+        }
+
+        this.query("KPU-Feeds-2014-07-17-03-AM-province.json", null, provinceCallback);
+    }
+
+    query(url, context?, callback?, statusCallback?) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            contentType: 'application/json',
+            context: context,
+            statusCode: statusCallback
+        }).always(function (data, status, jqXHR) {
+            if (callback) {
+                callback.call(this, data, status, jqXHR);
+            }
+        });
+    }
+}
+
+window.onload = () => {
+    var pilpres2014ViewModel = new Pilpres2014();
+    ko.applyBindings(pilpres2014ViewModel);
+
+    pilpres2014ViewModel.refresh();
+};
