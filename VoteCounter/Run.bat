cbf2ebb93ae5540a099f87bbe43efd95866c01a0
Henry Tan <ht4n@hotmail.com>
2014-07-19 19:15:54 -0700

    Add script for TaskScheduler

---
+++ VoteCounter/Run.bat
@@ -0,0 +1,16 @@
+﻿touch logging.txt
+move logging.txt logging.previous.txt
+VoteCounter.exe areacodetable.csv C:\GitHub\Pilpres2014Portal 16 > logging.txt
+
+TotalTrendAggregator.exe c:\GitHub\Pilpres2014Portal C:\GitHub\Pilpres2014Portal\totaltrend.tsv
+
+pushd C:\GitHub\Pilpres2014Portal
+git add KPU-Feeds*
+git add totaltrend.tsv
+git commit -m "new data"
+
+FeedsMetadataGenerator.exe C:\GitHub\Pilpres2014Portal C:\GitHub\Pilpres2014Portal\feedsources.json
+git add feedsources.json
+
+git push
+popd
